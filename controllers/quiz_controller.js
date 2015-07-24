c462b0d03ea6989a1b903d2efa60a25956f7abd4
Edison Quinatoa <edison.quinatoa@gmail.com>
2015-07-24 18:07:57 -0500

    Tarea modulo 6

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,15 @@
+/* quiz controller */
+
+// Question
+exports.question = function(req, res) {
+    res.render('quizes/question', {pregunta:'Capital de Italia.?'});
+};
+
+// Answer
+exports.answer = function(req, res) {
+    if (req.query.respuesta === 'Roma') {
+       res.render('quizes/answer', {respuesta: 'Correcto'});
+    } else {
+       res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    }
+};
